e01fd7155bbf690c050bd6caa8ab257df1c89794
全球喵喵 <quanqiumiaomiao@numademacbook-pro-2.local>
2016-03-21 13:34:23 +0800

    Initial Commit

---
+++ testshou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testshou
+//
+//  Created by 全球喵喵 on 16/3/21.
+//  Copyright © 2016年 全球喵喵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testshou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testshou
+//
+//  Created by 全球喵喵 on 16/3/21.
+//  Copyright © 2016年 全球喵喵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
